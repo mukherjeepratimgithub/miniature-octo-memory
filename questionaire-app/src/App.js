0dd7fd32d84e8e5c23613853bfeeb6566d4686d0
Pratim Mukherjee <pratimmukherjeeiem@gmail.com>
2021-06-06 00:23:47 +0530

    working with the components

--- questionaire-app/src/App.js
+++ questionaire-app/src/App.js
@@ -1,6 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 import Questions from './components/Questions'
+import quizQuestions from './api/quizQuestions';
+
+constructor(props) {
+  super(props);
+
+  this.state = {
+    counter: 0,
+    questionId: 1,
+    question: '',
+    answerOptions: [],
+    answer: '',
+    answersCount: {},
+    result: ''
+  };
+};
+componentDidMount() {
+  const shuffledAnswerOptions = quizQuestions.map((question) => this.shuffleArray(question.answers));  
+
+  this.setState({
+    question: quizQuestions[0].question,
+    answerOptions: shuffledAnswerOptions[0]
+  });
+}
+
+shuffleArray(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
 
 function App() {
   return (
